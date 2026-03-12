99d69b6bf45c41b17e36fa638679c768943b1d5e
23318dp <23318dp@gmaile.com>
2026-03-13 09:57:54 +1300

    13/03

--- pussumPlinko.js
+++ pussumPlinko.js
@@ -1,27 +1,30 @@
+	let score = 1;
+	let ball;
 function setup() {
 	console.log("setup: ");
 	cnv = new Canvas(1920,940);
-	circle = new Sprite(random(850,1000), 100, 30, 'd');
-	circle.color = color(255, 0,0); 
+	ball = new Sprite(random(850,1000), 100, 30, 'd');
+	ball.color = color(255, 0,0); 
 	world.gravity.y = 5;
+	
 	//Multipliers
-	pointFour = new Sprite(915, 590, 60, 30, 'k');
+	pointFour = new Sprite(915, 590, 60, 30, 's');
 	pointFour.color = color(255, 235,0); //yellow
-	pointSevenL = new Sprite(850, 590, 60, 30, 'k');
+	pointSevenL = new Sprite(850, 590, 60, 30, 's');
 	pointSevenL.color = color(255, 165,0); //light orange
-	 pointSevenR= new Sprite(980, 590, 60, 30, 'k');
+	 pointSevenR= new Sprite(980, 590, 60, 30, 's');
 	pointSevenR.color = color(255, 165,0); //light orange
-	oneThreeL= new Sprite(785, 590, 60, 30, 'k');
+	oneThreeL= new Sprite(785, 590, 60, 30, 's');
 	oneThreeL.color = color(255, 110,0); //dark orange
-	oneThreeR= new Sprite(1046, 590, 60, 30, 'k');
+	oneThreeR= new Sprite(1046, 590, 60, 30, 's');
 	oneThreeR.color = color(255, 110,0); //dark orange
-	threeL= new Sprite(720, 590, 60, 30, 'k');
+	threeL= new Sprite(720, 590, 60, 30, 's');
 	threeL.color = color(255, 50,0); //redish orange
-	threeR= new Sprite(1113, 590, 60, 30, 'k');
+	threeR= new Sprite(1113, 590, 60, 30, 's');
 	threeR.color = color(255, 50,0); //reddish orange
-	thirteenL= new Sprite(655, 590, 60, 30, 'k');
+	thirteenL= new Sprite(655, 590, 60, 30, 's');
 	thirteenL.color = color(255, 0,0); //red
-	thirteenR= new Sprite(1180, 590, 60, 30, 'k');
+	thirteenR= new Sprite(1180, 590, 60, 30, 's');
 	thirteenR.color = color(255, 0,0); //red
 	//plinks
 	for (var i = 0; i < 10; i++) {
@@ -72,9 +75,36 @@ function setup() {
 /*******************************************************/
 function draw() {
 	background(30, 35, 50);
-	text("Hello World", 915, 590) 
+	fill('red')
+	text(score, 915, 800);
+
+
+balls.forEach  
+	if (ball.overlaps(pointFour)) hitMultiplier(ball, 0.4);
+	if (ball.overlaps(pointSevenL)) hitMultiplier(ball, 0.7);
+	if (ball.overlaps(pointSevenR)) hitMultiplier(ball, 0.7);
+	
+	if (ball.overlaps(oneThreeL)) hitMultiplier(ball, 1.3);
+	if (ball.overlaps(oneThreeR)) hitMultiplier(ball, 1.3);
+	
+	if (ball.overlaps(threeL)) hitMultiplier(ball, 3);
+	if (ball.overlaps(threeR)) hitMultiplier(ball, 3);
+	
+	if (ball.overlaps(thirteenL)) hitMultiplier(ball, 13);
+	if (ball.overlaps(thirteenR)) hitMultiplier(ball, 13);
 }
 
+function hitMultiplier(ball, value) {
+	score *= value;
+	ball.remove();
+  }
+  function spawnBall() {
+	ball = new Sprite(random(850,1000), 100, 30, 'd');
+	ball.color = color(255, 0,0); 
+  }
+  function mousePressed () {
+	spawnBall();
+  }
 /*******************************************************/
 //  END OF APP
 /**/
